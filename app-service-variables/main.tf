de48cb05b2b789c92464932d2b7a720b64edb62f
BhavaniJeyaraman <45459323+BhavaniJeyaraman@users.noreply.github.com>
2019-10-09 17:27:22 +0530

    Update main.tf

--- app-service-variables/main.tf
+++ app-service-variables/main.tf
@@ -1,37 +1,13 @@
-resource "azurerm_resource_group" "test" {
-  name     = "${var.resource-group-name}"
-  location = "${var.location}"
+provider "azurerm" {
+        version = "=1.27.0"
 }
-
-resource "azurerm_app_service_plan" "test" {
-  name                = "terraform-appserviceplan"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
+resource "azurerm_resource_group" "example" {
+        name = "Test_RG"
+        location = "southindia"
 }
-
-resource "azurerm_app_service" "test" {
-  name                = "${var.app-service-name}"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.test.id}"
-
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-
-  app_settings = {
-    "SOME_KEY" = "some-value"
-  }
-
-  connection_string {
-    name  = "Database"
-    type  = "SQLServer"
-    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
-  }
+resource "azurerm_virtual_network" "example" {
+        name = "Test_VNet"
+        resource_group_name = "${azurerm_resource_group.example.name}"
+        location = "${azurerm_resource_group.example.location}"
+        address_space = ["10.0.0.0/16"]
 }
